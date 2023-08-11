7edf863299bab2eb5d821fc39718af9c8fc754ec
kjdv91 <126703713+kjaramillo91@users.noreply.github.com>
2023-08-11 12:00:47 -0700

    delegates-intro

---
+++ DelegateBasicExample/DelegateBasicExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DelegateBasicExample
+{
+    public class Program
+    {
+        delegate void LogDel(string text, DateTime dateTime);
+        static void Main(string[] args)
+        {
+            Log logL = new Log();
+            
+            LogDel log = new LogDel(logL.LongTextToScreen);
+            Console.WriteLine("Please enter your name");
+
+            var name = Console.ReadLine();
+
+            log(name, DateTime.Now);
+            Console.ReadKey();
+        }
+
+
+       
+    }
+
+
+    public class Log
+    {
+        public void LongTextToScreen(string text, DateTime dateTime)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+    }
+}
